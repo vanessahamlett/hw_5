25130ab5efe186fbf9b37a070ed7858a7baec890
Vanessa Hamlett <70493568+vanessahamlett@users.noreply.github.com>
2021-11-05 09:10:46 -0500

    Add files via upload

---
+++ madlib.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <regex>
+
+void inputFunction(std::string word){
+    std::string in;
+    std::cout << "Enter a: " << word << std::endl;
+    std::cin >> in;
+}
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+
+    std::ifstream infile("example.txt");
+
+    std::vector<std::string> wordsToReplace;
+
+    std::string line;
+    std::regex rgx("{.*?}");
+    while (getline(infile, line)){
+        std::smatch word;
+        std::regex_search(line, rgx);
+        //std::cout << (word[0]) << std::endl;
+            //inputFunction(word[1]);
+        
+    }
+
+    return 0;
+}
