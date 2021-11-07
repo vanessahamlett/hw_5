6f9b94d6312314aa74943fcc3902b7d69967a4d9
HullJac <jacobhull55@gmail.com>
2021-11-07 12:43:13 -0600

    done

--- madlib.cpp
+++ madlib.cpp
@@ -4,7 +4,7 @@
 
 std::string inputFunction(std::string word){
     std::string in;
-    std::cout << "Enter a: " << word << std::endl;
+    std::cout << "Enter a " << word << ": ";
     std::cin >> in;
     return in;
 }
@@ -15,12 +15,13 @@ int main(){
     std::ofstream outFile("out.txt");
 
     std::string line;
-    std::regex rgx("[{].*?}");
+    std::regex rgx("[{].*?[}]");
     while (getline(inFile, line)){
         std::smatch word;
-        std::regex_search(line, word, rgx);
-        std::string replaceWord = inputFunction(word[0]);
-        line = std::regex_replace(line, rgx, replaceWord);
+        while(std::regex_search(line, word, rgx)) {
+            std::string replaceWord = inputFunction(word[0]);
+            line = std::regex_replace(line, rgx, replaceWord, std::regex_constants::format_first_only); 
+        }
         outFile << line << "\n";
     }
 
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Our program reads in a file called example.txt and reads for curly braces using regex to find words to replace.
+It then takes input from the user and replaces the word surrounded by curly braces and writes the newline to an out.txt file.
+This input can only be a single continuious word.
